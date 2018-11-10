194ac2b13c65a80c72e2b4ea24158fe4906f10ca
Josh Campbell <josh@userdel.com>
2018-11-09 22:07:52 -0800

    adds foundation for the boto3 client and project creation

--- alpaca.py
+++ alpaca.py
@@ -7,13 +7,40 @@ compatability for programs using them on AWS Lambda.
 __author__ = "Josh Campbell"
 __version__ = "0.1.0"
 __license__ = "Apache2"
-from __future__ import print_function
 import boto3
+from botocore.exceptions import ClientError
+
+
+def create_build_project(client):
+    """ Creates a new AWS CodeBuild Project to build the pip package(s)"""
+    response = client.create_project(
+        name='alpacaBuilder',
+        source={
+            'type': 'NO_SOURCE',
+            'buildspec': '',
+        },
+        artifacts={
+            'type': 'NO_ARTIFACTs',
+        },
+        environment={
+            'type': 'LINUX_CONTAINER',
+            'image': 'aws/codebuild/python:3.6.5-1.3.1',
+            'computeType': 'BUILD_GENERAL1_SMALL',
+        },
+        serviceRole='',
+    )
+
+
+def create_client():
+    """ Creates a new boto3 client """
+    return boto3.client('codebuild')
 
 
 def main():
     """ Main entry point of the app """
-    print("blorp!")
+    client = create_client()
+    create_build_project(client)
+    print(client)
 
 
 if __name__ == "__main__":
