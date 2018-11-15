b89df8237edf90863e1bfc3fc1cbef8f41de5f14
Josh Campbell <josh@userdel.com>
2018-11-15 15:26:26 -0800

    initial commit

---
+++ alpaca/s3.py
@@ -0,0 +1,50 @@
+"""
+Administration of Amazon S3 Resources through a boto3 client.
+"""
+
+
+def download_artifact(resource, bucket, key='alpacaBuilder/alpaca.zip',
+                      local_path='.'):
+    """
+    Fetches the artifact from Amazon S3 built by Alpaca .
+
+    Parameters
+    ----------
+    resource : boto3.resources.factory.s3.ServiceResource
+        A boto3 Resource interface for S3.
+    bucket : str
+        Name of the bucket the artifact exists in.
+    key: str
+        The S3 key of the artifact to download from the bucket. Defaults to
+        "alpacaBuilder/alpaca.zip".
+    local_path: str
+        The local path where the artifact will be downloaded. Defaults to the
+        current directory Alpaca is being run from (i.e. ".").
+
+    Returns
+    -------
+    """
+    print("Downloading zip to {}...".format(local_path))
+    resource.meta.client.download_file(bucket, key, local_path)
+    print("Download complete...")
+
+
+def delete_artifact(client, bucket, key='alpacaBuilder/alpaca.zip'):
+    """
+    Deletes the artifact from Amazon S3 built by Alpaca.
+
+    Parameters
+    ----------
+    client : botocore.client.S3
+        A boto3 client for S3.
+    bucket : str
+        Name of the bucket the artifact exists in.
+    key: str
+        The S3 key of the artifact to delete from the bucket. Defaults to
+        "alpacaBuilder/alpaca.zip".
+
+    Returns
+    -------
+    """
+    print("Deleting {} from S3 bucket {}...".format(key, bucket))
+    client.delete_object(Bucket=bucket,Key=key)
