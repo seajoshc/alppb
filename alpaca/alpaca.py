4d3e02d3e35e6d21f7ccc5ea6fe39e19bf8d2301
Josh Campbell <josh@userdel.com>
2018-11-15 15:31:19 -0800

    uses the new s3 module for managing download and deletion of artifact

--- alpaca/alpaca.py
+++ alpaca/alpaca.py
@@ -6,28 +6,48 @@ Builds Python modules on Amazon Linux using AWS CodeBuild and downloads them.
 __author__ = "Josh Campbell"
 __version__ = "0.1.0"
 __license__ = "MIT"
-import subprocess
 import time
 import boto3
 import codebuild
 import iam
+import s3
 
 
-DOWNLOAD_ARTIFACT = "aws s3 cp s3://rebukethe.net/alpacaBuilder/alpaca.zip ."
-DELETE_ARTIFACT = "aws s3 rm s3://rebukethe.net/alpacaBuilder/alpaca.zip"
-
-
-def create_client(resource):
+def create_client(service):
     """ Creates a new boto3 client """
-    print("Creating boto3 client for {}...".format(resource))
-    return boto3.client(resource)
+    print("Creating boto3 client for {}...".format(service))
+    return boto3.client(service)
+
+
+def create_resource(service):
+    """
+    Creates a boto3 resource object for the specified service.
+
+    Parameters
+    ----------
+    service : str
+        The name of the AWS service to create the resource for.
+
+    Returns
+    -------
+    boto3.resources.factory.service.ServiceResource
+    """
+    return boto3.resource(service)
 
 
 def main():
     """ Main entry point of the app """
     print("Starting alpaca...")
+    # Create boto3 clients/resources used below.
     iam_client = create_client('iam')
     codebuild_client = create_client('codebuild')
+    s3_resource = create_resource('s3')
+    s3_client = create_client('s3')
+
+    # Hardcoded bucket during development.
+    bucket = 'rebukethe.net'
+
+    # Create Alpaca resources.
     role = iam.create_role(iam_client)
     # TODO be smarter about checking if the role is ready
     print("Waiting 10 seconds for IAM Role propagation before continuing...")
@@ -36,12 +56,12 @@ def main():
     codebuild.build_artifact(codebuild_client)
 
     # Download the artifact.
-    subprocess.run(DOWNLOAD_ARTIFACT.split(' '))
+    s3.download_artifact(s3_resource, bucket)
 
     # Cleanup phase.
     codebuild.delete_build_project(codebuild_client)
     iam.delete_role(iam_client)
-    subprocess.run(DELETE_ARTIFACT.split(' '))
+    s3.delete_artifact(s3_client, bucket)
 
     print("Exiting...")
 
