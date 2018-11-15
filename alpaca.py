09882f583b2156b6540bfa548d9653a9fcbbbfd2
Josh Campbell <josh@userdel.com>
2018-11-14 22:15:28 -0800

    adds missing return in recursive get_artifact_locaion() call, cleans up output messages, cleans up main()

--- alpaca.py
+++ alpaca.py
@@ -38,12 +38,12 @@ def create_build_project(client, role):
         serviceRole=role,
     )
 
-    return str(response.get('project', {}).get('arn'))
+    return response
 
 
 def delete_build_project(client):
     """ Deletes an AWS CodeBuild project """
-    print("Deleting build project...")
+    print("Deleting build project and cleaning up...")
     client.delete_project(name="alpacaBuilder")
 
 
@@ -54,28 +54,27 @@ def get_artifact_location(client, build_id):
     # batch_get_builds() will return an array with one element.
     status = response.get('builds')[0].get('buildStatus')
     print("Build status is {}".format(status))
-    if  status == 'SUCCEEDED':
+    if status == 'SUCCEEDED':
         print("Build completed...")
         # Build is done, return the artifact location.
-        artifact_location = str(response.get('builds')[0].get('artifacts').get('location'))
-        print(">>>>>>Artifact Location is {}".format(artifact_location))
-        return artifact_location
+        return str(response.get('builds')[0].get('artifacts').get('location'))
     else:
         print("Build not done, waiting 10 seconds...")
         time.sleep(10)
         # Recursively call until the build is done.
-        get_artifact_location(client, build_id)
+        return get_artifact_location(client, build_id)
 
 
 def build_artifact(client):
     """ Start a build and get the location of the artifact """
-    print("Starting build project...")
+    print("Starting build job...")
     response = client.start_build(projectName='alpacaBuilder')
     build_id = str(response.get('build').get('id'))
     print("Build ID is {}".format(build_id))
     artifact_location = get_artifact_location(client, build_id)
+    print("Built module(s) located at {}".format(artifact_location))
 
-    return artifact_location
+    return str(artifact_location)
 
 
 def create_client():
@@ -89,11 +88,10 @@ def main():
     print("Starting alpaca...")
     client = create_client()
     role = str(subprocess.check_output(GET_AWS_ACCOUNT, shell=True).decode(encoding='UTF-8')).rstrip()
-    arn = create_build_project(client, role)
-    print("Build Project ARN is {}...".format(arn))
-    artifact_location = build_artifact(client)
-    print("Library location: {}".format(artifact_location))
+    create_build_project(client, role)
+    build_artifact(client)
     delete_build_project(client)
+
     print("Exiting...")
 
 
