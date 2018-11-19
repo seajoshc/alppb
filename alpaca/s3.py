2bf986a4218e53bd7f2db6383d06e147fced9007
Josh Campbell <josh@userdel.com>
2018-11-18 20:15:27 -0800

    adds bucket_region()

--- alpaca/s3.py
+++ alpaca/s3.py
@@ -48,3 +48,24 @@ def delete_artifact(client, bucket, key='alpacaBuilder/alpaca.zip'):
     """
     print("Deleting {} from S3 bucket {}...".format(key, bucket))
     client.delete_object(Bucket=bucket, Key=key)
+
+
+def bucket_region(client, bucket):
+    """
+    Gets the region where this bucket exists.
+
+    Parameters
+    ----------
+    client : botocore.client.S3
+        A boto3 client for S3.
+
+    Returns
+    -------
+    str
+        Region of the bucket.
+    """
+    response = client.get_bucket_location(Bucket=bucket)
+    location = response.get('LocationConstraint')
+    if location is None:
+        return 'us-east-1'
+    return str(location)
