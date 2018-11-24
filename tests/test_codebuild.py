4a03e63d83db3c95b1e078808632744ad50ec803
Josh Campbell <josh@userdel.com>
2018-11-24 11:17:38 -0800

    adds tests for determine_image

--- tests/test_codebuild.py
+++ tests/test_codebuild.py
@@ -1,7 +1,31 @@
 from alppb.codebuild import determine_image
 
+
+"""
+alppb.codebuild.determine_image()
+"""
+def test_determine_image_python27():
+    assert determine_image("2.7") == "irlrobot/alppb-python27"
+
+
 def test_determine_image_python36():
-    determined_imaged = determine_image("3.6")
-    expected_image = "irlrobot/alppb-python36"
-    print("DI: {}\nEI: {}".format(determined_imaged, expected_image))
-    assert determined_imaged is expected_image
+    assert determine_image("3.6") == "irlrobot/alppb-python36"
+
+
+def test_determine_image_python37():
+    assert determine_image("3.7") == "irlrobot/alppb-python37"
+
+
+def test_determine_image_default():
+    """ Fallback to the python 3.6 image """
+    assert determine_image("None") == "irlrobot/alppb-python36"
+
+
+def test_determine_image_none():
+    """ Fallback to the python 3.6 image """
+    assert determine_image(None) == "irlrobot/alppb-python36"
+
+
+def test_determine_image_invalid():
+    """ Fallback to the python 3.6 image """
+    assert determine_image("blah") == "irlrobot/alppb-python36"
